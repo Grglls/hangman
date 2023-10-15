7265fb25fc903ee99f9af2305a495ea1f58045db
Greg Ells <gregells@gmail.com>
2023-10-16 10:57:32 +1300

    First commit

---
+++ js/main.js
@@ -0,0 +1,69 @@
+/*------------------------- constants -------------------------*/
+///Object with list of words: 
+const WORDS = {
+
+};
+
+
+/*------------------------- state variables -------------------------*/
+const state = {
+    currentWord: null,
+    numberOfGuesses: null,
+    guessedLetters: null,
+    result: null,
+};
+
+
+/*------------------------- cached elements  -------------------------*/
+const element = {
+    message: document.querySelector('h2'),
+    playAgain: document.getElementById(''),
+
+}
+
+/*------------------------- event listeners -------------------------*/
+
+
+/*------------------------- functions -------------------------*/
+init();
+
+
+// To-do: init function:
+function init () {
+
+}
+
+// To-do: handleClick function:
+function handleClick(event) {
+
+}
+
+
+// To-do: checkWinner function:
+function checkWinner(arguments) {
+
+}
+
+
+// To-do: render function:
+function render() {
+    renderBoard();
+    renderMessage();
+    renderControls();
+}
+
+
+// To-do: renderBoard function:
+function renderBoard() {
+
+}
+
+// To-do: renderMessage function:
+function renderMessage() {
+    
+}
+
+// To-do: renderControls function:
+function renderControls() {
+    
+}
